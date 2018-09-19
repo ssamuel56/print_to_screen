ba77b9ceedc8937754ba3aaedffce5b012e434b4
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-09-19 14:35:45 -0400

    added some simple functions to display numbers 1-100

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -5,3 +5,35 @@ def world
 end
 
 world
+
+puts 1 + 5
+
+puts ((16 + 9) * 15)
+
+puts "1" + "1"
+
+puts "What lovely weather we're having. " + "Just another beautiful day"
+
+100.times do
+	print 1
+end
+
+def adding(a, b)
+	a + b 
+end
+puts "\n"
+puts adding(12, 1)
+
+puts '.' * 10
+
+(1..100).each do |i|
+	puts i
+end
+
+number = 1
+
+until number > 100
+	puts number 
+	number += 1
+end
+
